6d4bc162d89e23abc623a62a605a4d33709708c0
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 18:07:26 -0700

    done code

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -9,9 +9,12 @@ import arithmetic
 def utilize_userinput():
     operation = raw_input("Please enter the calculation you want>>>")
 # asks user to input the operation they want
-    operation = operation.split(" ")
+    operation = operation.split(" ") 
+
     num1 = int(operation[1])
-    num2 = int(operation[2]) # accepts integers
+    if len(operation) > 2:
+        num2 = int(operation[2]) # accepts integers
+        
     if operation[0] == "add":
         return arithmetic.add(num1, num2)
     elif operation[0] == "subtract":
@@ -23,22 +26,29 @@ def utilize_userinput():
     elif operation[0] == "power":
         return arithmetic.power(num1, num2)
     elif operation[0] == "square":
-        return arithmetic.square(num1, num2)
+        return arithmetic.square(num1)
     elif operation[0] == "cube":
-        return arithmetic.cube(num1, num2)
+        return arithmetic.cube(num1)
     elif operation[0] == "mod":
         return arithmetic.mod(num1, num2)
     else:
         print "Please enter valid operation."
-        utilize_userinput()
-# if statements to check for valid operations, if incorrect continue to ask
+        
+        
+            
+    # if statements to check for valid operations, if incorrect continue to ask
 
-print utilize_userinput()
+       
+def try_again():
+    while True:
+        print "Would you like to play?"
+        answer = raw_input("Please enter Y for Yes or N for No>>>")
+        if answer == "Y":
+            print utilize_userinput()
+        else:
+            print "Thank you!"
+            break
+
+try_again()
 
 
-    
-
-
-
-
-# Your code goes here
